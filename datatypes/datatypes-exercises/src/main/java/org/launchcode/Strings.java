e6de9d8e7bdf024ee2a540379136c6d3ba83a971
Tatiana Snook <tatsnook@gmail.com>
2024-09-19 14:56:08 -0500

    Initial commit

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/Strings.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/Strings.java
@@ -14,8 +14,17 @@ public class Strings {
         System.out.println("Enter any search term for first sentence of Alice’s Adventures in Wonderland?");
         String searchTerm = input.nextLine();
 
+
         if (firstSentence.toLowerCase().contains(searchTerm.toLowerCase())) {
             System.out.println("Your search term was found: True");
+
+            Integer index = firstSentence.indexOf(searchTerm);
+            Integer length = searchTerm.length();
+            System.out.println("Index of your search term is " + index);
+            System.out.println("And the length of your search term is " + length + "characters long.");
+
+            String modifiedSentence = firstSentence.replace(searchTerm, "");
+            System.out.println(modifiedSentence);
         } else {
             System.out.println("Your search term was not found: False");
         }
