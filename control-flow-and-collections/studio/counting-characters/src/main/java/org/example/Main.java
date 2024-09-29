c68918b57f510fd2a368a1d675f55f2795d2e638
Tatiana Snook <tatsnook@gmail.com>
2024-09-29 06:15:38 -0500

    studio is completed

--- control-flow-and-collections/studio/counting-characters/src/main/java/org/example/Main.java
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/example/Main.java
@@ -2,25 +2,36 @@ package org.example;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String myString = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+//        String myString = "If the product of two terms is zero then common sense says at least one of the two terms" +
+//                "has to be zero to start with. So if you move all the terms over to one side, you can put the" +
+//                "quadratics into a form that can be factored allowing that side of the equation to equal zero." +
+//                "Once you’ve done that, it’s pretty straightforward from there.";
+        Scanner input = new Scanner(System.in);
+        System.out.println("Give me a sting to count: ");
+        String myString = input.nextLine().toLowerCase();
+
         char[] charactersInString = myString.toCharArray();
-//        System.out.println(Arrays.toString(charactersInString));
 
         HashMap<Character, Integer> characterCount = new HashMap<>();
 
-        for(char character: charactersInString){
-            if(!characterCount.containsKey(character)) {
-                characterCount.put(character, 1);
+        for(char letter: charactersInString){
+            if(Character.isLetter(letter)){
+                if(characterCount.containsKey(letter)) {
+                    characterCount.put(letter, characterCount.get(letter) + 1);
+                } else {
+                    characterCount.put(letter, 1);
+                }
             }
-            else {
-                characterCount.put(character, characterCount.get(character) + 1);
-            }
-
         }
-        System.out.println(characterCount);
+
+        for(Map.Entry<Character, Integer> charCount : characterCount.entrySet()) {
+            System.out.println(charCount.getKey() + ": " + charCount.getValue());
+        }
     }
 }
 
